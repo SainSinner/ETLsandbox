43849029a4923c6baf8c3e0bf8fac65207ff7139
SainSinner <g.svyat@yandex.ru>
2025-11-15 00:12:56 +0500

    create ETLsandbox

---
+++ scripts/ETLsandbox_my_scripts/Create table for orders_stream.sql
@@ -0,0 +1,30 @@
+-- Для схемы ниже собираем таблицу для хранения данных
+-- root
+--  |-- key_id: long (nullable = true)
+--  |-- operation: string (nullable = true)
+--  |-- id: long (nullable = true)
+--  |-- order_id: long (nullable = true)
+--  |-- status: string (nullable = true)
+--  |-- ts_data: long (nullable = true)
+--  |-- ts_event_ns: long (nullable = true)
+DROP view IF EXISTS default.orders_stream_cur;
+DROP TABLE IF EXISTS default.orders_stream;
+CREATE TABLE IF NOT EXISTS default.orders_stream (
+    key_id BIGINT,
+    operation String,
+    id BIGINT,
+    order_id BIGINT,
+    status String,
+    ts_data BIGINT,
+    ts_event_ns BIGINT
+)
+ENGINE = MergeTree()
+ORDER BY (key_id, ts_data);
+
+-- 1. Найти все временные таблицы dbt
+SELECT
+    'Drop table if exists ' || database || '.' || name || ';\n'
+    || 'DETACH TABLE ' || database || '.' || name || ';' as "Query"
+FROM system.tables
+WHERE database = 'default'
+  AND name LIKE '__dbt_exchange_test_%';
